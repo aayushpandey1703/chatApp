b6f54c2ea357448c46c9f3ebcc3b6a64d62b313c
Aayush pandey <pandeyaayush81@gmail.com>
2022-07-07 20:15:14 +0530

    location sharing

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,25 +1,36 @@
-const socket=io()
+const socket = io();
 
-socket.on('message',(message)=>{
-    console.log(message)    
-})
+socket.on("message", (message) => {
+  console.log(message);
+});
 
-$('#sendMessage').on('submit',(e)=>{
-    e.preventDefault()
-    var msg=$('#message').val()
-    if(msg)
-        socket.emit('message',msg)
-})
+$("#sendMessage").on("submit", (e) => {
+  e.preventDefault();
+  var msg = $("#message").val();
+  if (msg) {
+    socket.emit("message", msg, (response) => {
+      console.log(response);
+    });
+  }
+});
 
-socket.on('increment',(count)=>{
-    document.getElementById('count').innerHTML="<b style='color:red'>"+count+"</b> users online"
-})
+socket.on("increment", (count) => {
+  document.getElementById("count").innerHTML =
+    "<b style='color:red'>" + count + "</b> users online";
+});
 
-// socket.on('countUpdate',(count)=>{
-//     console.log('the count has updated',count)
-// })
-
-// // send message from client
-// $('#increment').on('click',()=>{
-//     socket.emit('increment')
-// })
+$("#send-location").on("click", () => {
+  if (!navigator.geolocation) return alert("Your doesnt support");
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        console.log("Location shared!");
+      }
+    );
+  });
+});
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express=require('express')
 const path=require('path')
 const http=require('http')
 const socketio=require('socket.io')
+const Filter=require('bad-words')
+const filter=new Filter()
+
 
 const app=express()
 const server=http.createServer(app)
@@ -17,15 +20,25 @@ let count=0
 io.on('connection',(socket)=>{
     count++
     io.emit('increment',count)
-    console.log('new WebSocket connection')
 
     socket.emit('message','Welcome!')
 
     socket.broadcast.emit('message','new user entered chat')
     
     // listen to client
-    socket.on('message',(message)=>{
+    socket.on('message',(message,callback)=>{
+        const check=filter.isProfane(message)
+        if(check)
+            return callback('Please dont use abusive language')
         io.emit('message',message)
+        callback('Delivered!')
+
+   
+    })
+
+    socket.on('sendLocation',(position,callback)=>{
+        io.emit('message','https://google.com/maps?q='+position.latitude+','+position.longitude)
+        callback()
     })
 
     socket.on('disconnect',()=>{
@@ -35,6 +48,7 @@ io.on('connection',(socket)=>{
     })
 
 })
+
 
 server.listen(port,()=>{
     console.log('server up on port '+port)
